2a23dcaed456c757ed1eb89e4979b346d9a53dd5
Douglas Pereira <douglasrodrigues20161@outlook.com>
2024-06-22 12:34:56 -0300

    criando testes utilizando Asserts

---
+++ 01 - Testes de Unidade/Demo.Tests/AssertStringsTests.cs
@@ -0,0 +1,84 @@
+﻿namespace Demo.Tests
+{
+    public class AssertStringsTests
+    {
+
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Douglas", "Pereira");
+
+            // Assert
+            Assert.Equal("Douglas Pereira", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveIgnorarCase()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Douglas", "Pereira");
+
+            // Assert
+            Assert.Equal("DOUGLAS Pereira", nomeCompleto, true);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveConterTrecho()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Douglas", "Pereira");
+
+            // Assert
+            Assert.Contains("glas", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveComecarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Douglas", "Pereira");
+
+            // Assert
+            Assert.StartsWith("Dou", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveAcabarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Douglas", "Pereira");
+
+            // Assert
+            Assert.EndsWith("ira", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_ValidarExpressaoRegular()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Douglas", "Pereira");
+
+            // Assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+        }
+    }
+}
---
+++ 01 - Testes de Unidade/Demo.Tests/AssertingExceptionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo.Tests
+{
+    public class AssertingExceptionsTests
+    {
+        [Fact]
+        public void Calculadora_Dividir_DeveRetornarErroDivisaoPorZero()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act & Assert
+            Assert.True(Double.IsInfinity(calculadora.Dividir(10, 0)));
+        }
+
+
+        [Fact]
+        public void Funcionario_Salario_DeveRetornarErroSalarioInferiorPermitido()
+        {
+            // Arrange & Act & Assert
+            var exception =
+                Assert.Throws<Exception>(() => FuncionarioFactory.Criar("Douglas", 250));
+
+            Assert.Equal("Salario inferior ao permitido", exception.Message);
+        }
+    }
+}
