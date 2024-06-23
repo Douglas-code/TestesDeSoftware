9e4d7653828e9b43fc65756b8691b619d8fb81bc
Douglas Pereira <douglasrodrigues20161@outlook.com>
2024-06-23 16:10:48 -0300

    finalizando testes do metodo AdicionarItem e testando/criando AtualizarItem e Remover Item

--- 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -1,7 +1,12 @@
-﻿namespace NerdStore.Vendas.Domain
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Vendas.Domain
 {
     public class Pedido
     {
+        public static int MAX_UNIDADES_ITEM => 15;
+        public static int MIN_UNIDADES_ITEM => 1;
+
         protected Pedido()
         {
             _pedidoItems = new List<PedidoItem>();
@@ -10,6 +15,8 @@
         public Guid ClienteId { get; private set; }
 
         public decimal ValorTotal { get; private set; }
+
+        public decimal Desconto { get; private set; }
 
         public PedidoStatus PedidoStatus { get; private set; }
 
@@ -26,6 +33,11 @@
             return _pedidoItems.Any(p => p.ProdutoId == item.ProdutoId);
         }
 
+        private void ValidarPedidoItemInexistente(PedidoItem item)
+        {
+            if (!PedidoItemExistente(item)) throw new DomainException("O item não pertence ao pedido");
+        }
+
         private void ValidarQuantidadeItemPermitida(PedidoItem item)
         {
             var quantidadeItems = item.Quantidade;
@@ -34,6 +46,8 @@
                 var itemExistente = _pedidoItems.FirstOrDefault(p => p.ProdutoId == item.ProdutoId);
                 quantidadeItems += itemExistente.Quantidade;
             }
+
+            if (quantidadeItems > MAX_UNIDADES_ITEM) throw new DomainException($"Máximo de {MAX_UNIDADES_ITEM} unidades por produto.");
         }
 
         public void AdicionarItem(PedidoItem pedidoItem)
@@ -50,6 +64,28 @@
             }
 
             _pedidoItems.Add(pedidoItem);
+            CalcularValorPedido();
+        }
+
+        public void AtualizarItem(PedidoItem pedidoItem)
+        {
+            ValidarPedidoItemInexistente(pedidoItem);
+            ValidarQuantidadeItemPermitida(pedidoItem);
+
+            var itemExistente = PedidoItems.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId);
+
+            _pedidoItems.Remove(itemExistente);
+            _pedidoItems.Add(pedidoItem);
+
+            CalcularValorPedido();
+        }
+
+        public void RemoverItem(PedidoItem pedidoItem)
+        {
+            ValidarPedidoItemInexistente(pedidoItem);
+
+            _pedidoItems.Remove(pedidoItem);
+
             CalcularValorPedido();
         }
 
