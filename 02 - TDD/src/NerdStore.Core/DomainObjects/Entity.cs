dd1a14349cc3eeb1f49269a344475a6d148552f5
Douglas Pereira <douglasrodrigues20161@outlook.com>
2024-06-23 17:33:31 -0300

    adicionando estrura do SharedKernel e adicionando classes compartilhadas

---
+++ 02 - TDD/src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,33 @@
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        private List<Event> _eventos;
+        public IReadOnlyCollection<Event> Eventos => _eventos?.AsReadOnly();
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public void AdicionarEvento(Event evento)
+        {
+            _eventos = _eventos ?? new List<Event>();
+            _eventos.Add(evento);
+        }
+
+        public void RemoverEvento(Event eventItem)
+        {
+            _eventos?.Remove(eventItem);
+        }
+
+        public void LimparEventos()
+        {
+            _eventos?.Clear();
+        }
+    }
+}
