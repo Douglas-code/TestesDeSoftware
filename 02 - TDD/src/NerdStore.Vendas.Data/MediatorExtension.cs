8b42b6ddb2fbf578f2bfcc43db8d79d073a5cb79
Douglas Pereira <douglasrodrigues20161@outlook.com>
2024-06-23 18:29:58 -0300

    criando testes command e command handler

---
+++ 02 - TDD/src/NerdStore.Vendas.Data/MediatorExtension.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Vendas.Data
+{
+    public static class MediatorExtension
+    {
+        public static async Task PublicarEventos(this IMediator mediator, VendasContext ctx)
+        {
+            var domainEntities = ctx.ChangeTracker
+                .Entries<Entity>()
+                .Where(x => x.Entity.Eventos != null && x.Entity.Eventos.Any());
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.Eventos)
+                .ToList();
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.LimparEventos());
+
+            var tasks = domainEvents
+                .Select(async (domainEvent) => {
+                    await mediator.Publish(domainEvent);
+                });
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
